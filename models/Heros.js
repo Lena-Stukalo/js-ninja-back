c4e12e377bc4df8221609b37fd87ed57ac5a4fbd
Lena Stukalo <68201887+Lena-Stukalo@users.noreply.github.com>
2023-05-25 13:45:33 +0300

    settings

---
+++ models/Heros.js
@@ -0,0 +1,19 @@
+// const fs = require('fs/promises')
+
+const listHeros = async () => {}
+
+const getHeroById = async (heroId) => {}
+
+const removeHero = async (heroId) => {}
+
+const addHero = async (body) => {}
+
+const updateHero = async (heroId, body) => {}
+
+module.exports = {
+  listHeros,
+  getHeroById,
+  removeHero,
+  addHero,
+  updateHero,
+}
